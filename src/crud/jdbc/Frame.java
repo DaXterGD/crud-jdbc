938dacb29ae9bde50c44fafd37e04b9212e099a8
DaXterGD <faniel5431@gmail.com>
2023-10-02 21:45:31 -0500

    formulario creado

--- src/crud/jdbc/Frame.java
+++ src/crud/jdbc/Frame.java
@@ -30,11 +30,263 @@ public class Frame extends javax.swing.JFrame {
   private void initComponents() {
 
     layoutPanel = new javax.swing.JPanel();
+    title = new javax.swing.JLabel();
+    insertPanel = new javax.swing.JPanel();
+    insertPanelTitle = new javax.swing.JLabel();
+    idLabel = new javax.swing.JLabel();
+    idInput = new javax.swing.JTextField();
+    productNameLabel = new javax.swing.JLabel();
+    productNameInput = new javax.swing.JTextField();
+    categoryLabel = new javax.swing.JLabel();
+    categoryInput = new javax.swing.JTextField();
+    priceLabel = new javax.swing.JLabel();
+    priceInput = new javax.swing.JTextField();
+    saveButton = new javax.swing.JButton();
+    updateButton = new javax.swing.JButton();
+    deleteButton = new javax.swing.JButton();
+    cleanButton = new javax.swing.JButton();
+    tablePanel = new javax.swing.JPanel();
+    tablePanelTitle = new javax.swing.JLabel();
+    jScrollPane1 = new javax.swing.JScrollPane();
+    table = new javax.swing.JTable();
     background = new javax.swing.JLabel();
 
     setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
     layoutPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+    title.setBackground(new java.awt.Color(29, 28, 34));
+    title.setFont(new java.awt.Font("Josefin Sans", 1, 48)); // NOI18N
+    title.setForeground(new java.awt.Color(255, 255, 255));
+    title.setText("¡A Comernos Eso!");
+    title.setOpaque(true);
+    layoutPanel.add(title, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 20, -1, -1));
+
+    insertPanel.setBackground(new java.awt.Color(25, 24, 29));
+
+    insertPanelTitle.setBackground(new java.awt.Color(255, 255, 255));
+    insertPanelTitle.setFont(new java.awt.Font("Josefin Sans", 1, 24)); // NOI18N
+    insertPanelTitle.setForeground(new java.awt.Color(255, 255, 255));
+    insertPanelTitle.setText("Inserta un producto");
+
+    idLabel.setBackground(new java.awt.Color(23, 22, 26));
+    idLabel.setFont(new java.awt.Font("Josefin Sans", 0, 18)); // NOI18N
+    idLabel.setForeground(new java.awt.Color(255, 255, 255));
+    idLabel.setText("ID");
+
+    idInput.setFont(new java.awt.Font("Josefin Sans", 0, 18)); // NOI18N
+    idInput.setForeground(new java.awt.Color(40, 40, 40));
+    idInput.setName(""); // NOI18N
+    idInput.setOpaque(true);
+    idInput.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        idInputActionPerformed(evt);
+      }
+    });
+
+    productNameLabel.setBackground(new java.awt.Color(23, 22, 26));
+    productNameLabel.setFont(new java.awt.Font("Josefin Sans", 0, 18)); // NOI18N
+    productNameLabel.setForeground(new java.awt.Color(255, 255, 255));
+    productNameLabel.setText("Nombre del producto");
+
+    productNameInput.setFont(new java.awt.Font("Josefin Sans", 0, 18)); // NOI18N
+
+    categoryLabel.setBackground(new java.awt.Color(23, 22, 26));
+    categoryLabel.setFont(new java.awt.Font("Josefin Sans", 0, 18)); // NOI18N
+    categoryLabel.setForeground(new java.awt.Color(255, 255, 255));
+    categoryLabel.setText("Categoría");
+
+    categoryInput.setFont(new java.awt.Font("Josefin Sans", 0, 18)); // NOI18N
+
+    priceLabel.setBackground(new java.awt.Color(23, 22, 26));
+    priceLabel.setFont(new java.awt.Font("Josefin Sans", 0, 18)); // NOI18N
+    priceLabel.setForeground(new java.awt.Color(255, 255, 255));
+    priceLabel.setText("Precio");
+
+    priceInput.setFont(new java.awt.Font("Josefin Sans", 0, 18)); // NOI18N
+    priceInput.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        priceInputActionPerformed(evt);
+      }
+    });
+
+    saveButton.setFont(new java.awt.Font("Josefin Sans", 1, 14)); // NOI18N
+    saveButton.setText("Enviar");
+    saveButton.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        saveButtonActionPerformed(evt);
+      }
+    });
+
+    updateButton.setFont(new java.awt.Font("Josefin Sans", 1, 14)); // NOI18N
+    updateButton.setText("Modificar");
+    updateButton.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        updateButtonActionPerformed(evt);
+      }
+    });
+
+    deleteButton.setFont(new java.awt.Font("Josefin Sans", 1, 14)); // NOI18N
+    deleteButton.setText("Eliminar");
+    deleteButton.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        deleteButtonActionPerformed(evt);
+      }
+    });
+
+    cleanButton.setFont(new java.awt.Font("Josefin Sans", 1, 14)); // NOI18N
+    cleanButton.setText("Limpiar");
+    cleanButton.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        cleanButtonActionPerformed(evt);
+      }
+    });
+
+    javax.swing.GroupLayout insertPanelLayout = new javax.swing.GroupLayout(insertPanel);
+    insertPanel.setLayout(insertPanelLayout);
+    insertPanelLayout.setHorizontalGroup(
+      insertPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(insertPanelLayout.createSequentialGroup()
+        .addGap(32, 32, 32)
+        .addGroup(insertPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+          .addGroup(insertPanelLayout.createSequentialGroup()
+            .addGap(94, 94, 94)
+            .addComponent(insertPanelTitle)
+            .addContainerGap(137, Short.MAX_VALUE))
+          .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, insertPanelLayout.createSequentialGroup()
+            .addGroup(insertPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+              .addGroup(insertPanelLayout.createSequentialGroup()
+                .addComponent(idLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(idInput, javax.swing.GroupLayout.PREFERRED_SIZE, 217, javax.swing.GroupLayout.PREFERRED_SIZE))
+              .addGroup(insertPanelLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(insertPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                  .addComponent(productNameInput, javax.swing.GroupLayout.PREFERRED_SIZE, 217, javax.swing.GroupLayout.PREFERRED_SIZE)
+                  .addComponent(categoryInput, javax.swing.GroupLayout.PREFERRED_SIZE, 217, javax.swing.GroupLayout.PREFERRED_SIZE)))
+              .addGroup(insertPanelLayout.createSequentialGroup()
+                .addComponent(priceLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(priceInput, javax.swing.GroupLayout.PREFERRED_SIZE, 217, javax.swing.GroupLayout.PREFERRED_SIZE)))
+            .addGap(38, 38, 38))
+          .addGroup(insertPanelLayout.createSequentialGroup()
+            .addGroup(insertPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+              .addComponent(productNameLabel)
+              .addComponent(categoryLabel))
+            .addGap(0, 0, Short.MAX_VALUE))))
+      .addGroup(insertPanelLayout.createSequentialGroup()
+        .addGap(21, 21, 21)
+        .addComponent(saveButton, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+        .addComponent(updateButton, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+        .addComponent(deleteButton, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+        .addComponent(cleanButton, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addGap(0, 0, Short.MAX_VALUE))
+    );
+    insertPanelLayout.setVerticalGroup(
+      insertPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(insertPanelLayout.createSequentialGroup()
+        .addGap(18, 18, 18)
+        .addGroup(insertPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+          .addGroup(insertPanelLayout.createSequentialGroup()
+            .addGroup(insertPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+              .addGroup(insertPanelLayout.createSequentialGroup()
+                .addComponent(insertPanelTitle)
+                .addGap(44, 44, 44)
+                .addGroup(insertPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                  .addComponent(idInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                  .addComponent(idLabel))
+                .addGap(18, 18, 18)
+                .addComponent(productNameLabel))
+              .addComponent(productNameInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGap(18, 18, 18)
+            .addComponent(categoryLabel))
+          .addComponent(categoryInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+        .addGroup(insertPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+          .addComponent(priceLabel)
+          .addComponent(priceInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        .addGap(53, 53, 53)
+        .addGroup(insertPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+          .addComponent(saveButton)
+          .addComponent(updateButton)
+          .addComponent(deleteButton)
+          .addComponent(cleanButton))
+        .addContainerGap(18, Short.MAX_VALUE))
+    );
+
+    idInput.getAccessibleContext().setAccessibleName("inserta");
+
+    layoutPanel.add(insertPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 200, 490, 320));
+
+    tablePanel.setBackground(new java.awt.Color(25, 24, 29));
+
+    tablePanelTitle.setBackground(new java.awt.Color(23, 22, 26));
+    tablePanelTitle.setFont(new java.awt.Font("Josefin Sans", 1, 24)); // NOI18N
+    tablePanelTitle.setForeground(new java.awt.Color(255, 255, 255));
+    tablePanelTitle.setText("Productos registrados");
+
+    table.setFont(new java.awt.Font("Josefin Sans", 0, 12)); // NOI18N
+    table.setModel(new javax.swing.table.DefaultTableModel(
+      new Object [][] {
+        {null, null, null, null},
+        {null, null, null, null},
+        {null, null, null, null},
+        {null, null, null, null}
+      },
+      new String [] {
+        "ID", "Nombre del producto", "Categoría", "Precio"
+      }
+    ) {
+      Class[] types = new Class [] {
+        java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.Float.class
+      };
+      boolean[] canEdit = new boolean [] {
+        false, false, false, false
+      };
+
+      public Class getColumnClass(int columnIndex) {
+        return types [columnIndex];
+      }
+
+      public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return canEdit [columnIndex];
+      }
+    });
+    jScrollPane1.setViewportView(table);
+    if (table.getColumnModel().getColumnCount() > 0) {
+      table.getColumnModel().getColumn(0).setResizable(false);
+      table.getColumnModel().getColumn(1).setResizable(false);
+      table.getColumnModel().getColumn(2).setResizable(false);
+      table.getColumnModel().getColumn(3).setResizable(false);
+    }
+
+    javax.swing.GroupLayout tablePanelLayout = new javax.swing.GroupLayout(tablePanel);
+    tablePanel.setLayout(tablePanelLayout);
+    tablePanelLayout.setHorizontalGroup(
+      tablePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, tablePanelLayout.createSequentialGroup()
+        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        .addComponent(tablePanelTitle)
+        .addGap(235, 235, 235))
+      .addGroup(tablePanelLayout.createSequentialGroup()
+        .addGap(18, 18, 18)
+        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 694, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addContainerGap(18, Short.MAX_VALUE))
+    );
+    tablePanelLayout.setVerticalGroup(
+      tablePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(tablePanelLayout.createSequentialGroup()
+        .addGap(19, 19, 19)
+        .addComponent(tablePanelTitle)
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 529, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addContainerGap(16, Short.MAX_VALUE))
+    );
+
+    layoutPanel.add(tablePanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 100, 730, 600));
 
     background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/crud/jdbc/assets/background.jpg"))); // NOI18N
     background.setToolTipText("");
@@ -53,6 +305,30 @@ public class Frame extends javax.swing.JFrame {
 
     pack();
   }// </editor-fold>//GEN-END:initComponents
+
+  private void priceInputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_priceInputActionPerformed
+    // TODO add your handling code here:
+  }//GEN-LAST:event_priceInputActionPerformed
+
+  private void saveButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveButtonActionPerformed
+    // TODO add your handling code here:
+  }//GEN-LAST:event_saveButtonActionPerformed
+
+  private void updateButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_updateButtonActionPerformed
+    // TODO add your handling code here:
+  }//GEN-LAST:event_updateButtonActionPerformed
+
+  private void deleteButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteButtonActionPerformed
+    // TODO add your handling code here:
+  }//GEN-LAST:event_deleteButtonActionPerformed
+
+  private void cleanButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cleanButtonActionPerformed
+    // TODO add your handling code here:
+  }//GEN-LAST:event_cleanButtonActionPerformed
+
+  private void idInputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_idInputActionPerformed
+    // TODO add your handling code here:
+  }//GEN-LAST:event_idInputActionPerformed
 
   public static void main(String args[]) {
     /* Set the Nimbus look and feel */
@@ -88,6 +364,25 @@ public class Frame extends javax.swing.JFrame {
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
   private javax.swing.JLabel background;
+  private javax.swing.JTextField categoryInput;
+  private javax.swing.JLabel categoryLabel;
+  private javax.swing.JButton cleanButton;
+  private javax.swing.JButton deleteButton;
+  private javax.swing.JTextField idInput;
+  private javax.swing.JLabel idLabel;
+  private javax.swing.JPanel insertPanel;
+  private javax.swing.JLabel insertPanelTitle;
+  private javax.swing.JScrollPane jScrollPane1;
   private javax.swing.JPanel layoutPanel;
+  private javax.swing.JTextField priceInput;
+  private javax.swing.JLabel priceLabel;
+  private javax.swing.JTextField productNameInput;
+  private javax.swing.JLabel productNameLabel;
+  private javax.swing.JButton saveButton;
+  private javax.swing.JTable table;
+  private javax.swing.JPanel tablePanel;
+  private javax.swing.JLabel tablePanelTitle;
+  private javax.swing.JLabel title;
+  private javax.swing.JButton updateButton;
   // End of variables declaration//GEN-END:variables
 }
